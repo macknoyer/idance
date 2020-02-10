3e9d1ae0d23e67332b8bf3524130253715aec3e0
Yudin Nikita <yudin.nc@gmail.com>
2020-02-10 17:44:29 +0500

    events-single

---
+++ events-single.php
@@ -0,0 +1,62 @@
+<?php include('templates/header_page.php');?>
+			<!-- 
+			=============================================
+				Theme Inner Banner
+			============================================== 
+			-->
+			<div class="theme-inner-banner">
+				<img src="images/shape/13.png" alt="" class="bg-shape">
+				<!-- <img src="images/shape/dancer3.png" alt="" class="shape-one">
+				<img src="images/shape/dancer.png" alt="" class="shape-two"> -->
+				<!-- <img src="images/shape/dancer2.png" alt="" class="shape-three"> -->
+				<div class="container">
+					<h2 class="title">MIX DANCE KIDS 5-6 YEARS OLD</h2>
+				</div> <!-- /.container -->
+			</div> <!-- /.theme-inner-banner -->
+
+
+			<!--
+			=====================================================
+				Blog List
+			=====================================================
+			-->
+			<div class="section-spacing section-spacing-bottom">
+				<div class="container">
+					<div class="row">
+						<div class="col-12 blog-details">
+							
+							<div class="single-blog-post">
+								<div class="img-box"><img src="images/blog/kids-dance-classes-brooklyn.jpg" alt=""></div>
+								<div class="post-meta">
+									<ul class="date-meta">
+										<li><i class="fa fa-clock-o" aria-hidden="true"></i> October 22, 2018</li>
+									</ul>
+									<h2 class="blog-title">Musical Theatre is a performance based class focusing on jazz</h2>
+									<p>Musical Theatre is a performance-based class focusing on jazz technique and skills instrumental in Broadway-style choreography. Ballet and classical jazz exercises will be emphasized during center floor warm up. Across the floor exercises will develop a variety of turns and leaps. Students will learn original choreography from some of Broadway's most celebrated choreographers, covering a wide range of musical theatre eras and styles</p>
+									<p>Modern dance is a form of movement that draws upon ballet, jazz, yoga, Pilates, and many more styles of dance and athletics. Modern is focused on creating new and unique forms of movement and expression, without the codified steps required in ballet. Certain stylized forms of modern that will be introduced in class include techniques from visionaries such as Alvin Ailey, </p>
+									<blockquote>Students experience a variety of tap styles with strong emphasis on technique and performance. </blockquote>
+									<p>Instructors focus on the rhythm tap style, with influences of tap masters such as Brenda Bufalino and Sam Weber. Our tap training can begin as early as Kindergarten age, where students are introduced to elementary rhythms and use of the body, legs and feet in basic tap steps. As students mature and progress, increasingly demanding technical exercises and complex routines are taught. Students are given some musical and historical context for better understanding of jazz music and tap dance as an American artistic expression</p>
+									<ul class="tags">
+										<li><a href="#">dances</a></li>
+										<li><a href="#">marketing</a></li>
+										<li><a href="#">development</a></li>
+									</ul>
+
+									<div class="clearfix bottom-widget">
+										<ul class="social-icon">
+											<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+											<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+											<li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+											<li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+										</ul>
+									</div>
+								</div> <!-- /.post-meta -->
+							</div> <!-- /.single-blog -->
+						</div> <!-- /.blog-list -->
+					</div> <!-- /.row -->
+				</div> <!-- /.container -->
+			</div>
+			
+			
+
+<?php include('templates/footer.php');?>
